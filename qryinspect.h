f2eca93735c5625539e66620f15c9d1e75ace0da
deprao <66636811+deprao@users.noreply.github.com>
2020-07-17 17:09:45 -0300

    Delete qryinspect.h

--- qryinspect.h
+++
@@ -1,7 +0,0 @@
-void intersect(int i,int j,node *list,FILE *out);
-void intcircrect(node *list,circ c,rect r,FILE *out);
-void intcircle(node *list,circ c1,circ c2,FILE *out);
-void intrectangle(node *list,rect r1,rect r2,FILE *out);
-void intern(int i,double x,double y,node *list,FILE *out);
-void intpointcircle(double x,double y,node *list,circ c,FILE *out);
-void intpointrect(double x,double y,node *list,rect r,FILE *out);
